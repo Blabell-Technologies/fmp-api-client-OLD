4fa12fd625ab99781dfc4b1acde1bc24b9311742
santi28 <santidenicolas@gmail.com>
2021-02-14 00:28:41 -0300

    Añadido JSDoc y estandar de respuesta

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,6 @@
 const FMP = require('../lib/fmp');
 
-console.log('To create a new instance of FMP use\n\nconst fmp = new FMP("your-token")');
+// const fmp = new FMP()
+// const search = await fmp.pets.edit(
+//   'c454c705-753e-49d3-abd7-f5d65c90431d',
+// );
