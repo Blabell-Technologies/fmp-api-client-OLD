e342b9c0e899cd7e2ac9809a65975424819b6cd2
santi28 <santidenicolas@gmail.com>
2021-02-15 17:28:27 -0300

    Correción de bugs en busqueda

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,4 @@
 const FMP = require('../lib/fmp');
 
-// const fmp = new FMP()
-// const search = await fmp.pets.edit(
-//   'c454c705-753e-49d3-abd7-f5d65c90431d',
-// );
+const fmp = new FMP()
+const search = await fmp.pets.nearby();
