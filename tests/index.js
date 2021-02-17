73add178837666155cc96de510fade74095d99b8
santi28 <santidenicolas@gmail.com>
2021-02-17 15:57:39 -0300

    feat: Metodo de borrado de mascotas

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,8 @@
 const FMP = require('../lib/fmp');
+(async () => {
+  const fmp = new FMP()
+  try { var del = await fmp.pets.delete('8ef1bf04-fb2e-443c-a90e-b9360ac0d474'); }
+  catch (err) { return console.error(err); }
 
-const fmp = new FMP()
-const search = await fmp.pets.nearby();
+  console.log(del);
+})()
